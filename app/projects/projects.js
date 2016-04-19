abd403f4ed2f3a2386e6a80b9660750509c7fc47
terziev-viktor <terzievviktor@gmail.com>
2016-04-19 22:39:59 +0300

    Added function for getting projects by given filter.

--- app/projects/projects.js
+++ app/projects/projects.js
@@ -42,10 +42,31 @@ angular.module('IssueTracker.projects', [])
             });
             return def.promise;
         }
+
+        /*
+        * Security: Logged in
+        * o	filter (String): the filters which you want the projects to be filtered by
+         	Supports every projects property with equals, less (or equal) than, greater (or equal)
+            than comparators (for example “Name == “SIT Project””)
+         	Supports child properties (for example: “Lead.Id == “e980a9d8-53e5-4f6b-b8ae-1efec2e58938””)
+         	Supports multiple criterias using “and” and “or” in between them (for example “Lead.Username == "admin@softuni.bg" or
+            Description.Contains("test"))
+         *
+        * */
+        function getProjectsByFilter(filter, pageSize, pageNumber) {
+            var def = $q.defer();
+            $http.get(BASE_URL + '/Projects/?pageSize={'+pageSize+'}&pageNumber={'+pageNumber+'}&' + filter).then(function (response) {
+                def.resolve(response);
+            }, function (error) {
+                def.reject(error)
+            });
+            return def.promise;
+        }
         return {
             getAllProjects: getAllProjects,
             getProjectById: getProjectById,
             postProject: postProject,
-            editProject: editProject
+            editProject: editProject,
+            getProjectsByFilter: getProjectsByFilter
         }
     }]);
