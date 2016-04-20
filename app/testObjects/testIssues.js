b1f715ba620e24e2fd18e16968565f5772a4803f
terziev-viktor <terzievviktor@gmail.com>
2016-04-20 19:20:53 +0300

    Added fake issue objects that can be used for testing.

---
+++ app/testObjects/testIssues.js
@@ -0,0 +1,56 @@
+var testIssues = [
+    {
+        title: 'issue_1',
+        description: 'issue_1 description',
+        project: 'issue_1 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_2',
+        description: 'issue_2 description',
+        project: 'issue_2 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_3',
+        description: 'issue_3 description',
+        project: 'issue_4 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_4',
+        description: 'issue_4 description',
+        project: 'issue_4 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_5',
+        description: 'issue_5 description',
+        project: 'issue_5 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_6',
+        description: 'issue_6 description',
+        project: 'issue_6 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_7',
+        description: 'issue_7 description',
+        project: 'issue_7 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_77',
+        description: 'issue_77 description',
+        project: 'issue_77 project',
+        duedate: new Date()
+    },
+    {
+        title: 'issue_66',
+        description: 'issue_66 description',
+        project: 'issue_66 project',
+        duedate: new Date()
+    }
+];
