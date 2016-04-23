810794b99fd7fdbeb7e1173ab642bfe990f079e1
terziev-viktor <terzievviktor@gmail.com>
2016-04-23 17:03:54 +0300

    Viewing issue is now currect.

--- app/viewIssuePage/viewIssuePage.js
+++ app/viewIssuePage/viewIssuePage.js
@@ -9,6 +9,28 @@ angular.module('IssueTracker.issuePage', ['ngRoute'])
         });
     }])
 
-    .controller('IssuePageCtrl', ['$scope', '$routeProvider', '$http', function($scope, $routeProvider, $http) {
+    .controller('IssuePageCtrl', ['$scope', '$routeParams', 'issues', function($scope, $routeParams, issues) {
+        var issueId = $routeParams.id;
+        issues.getIssueById(issueId).then(function (r) {
+            var response = r.data;
+            $scope.IssueKey = response.IssueKey;
+            $scope.Title = response.Title;
+            $scope.Description = response.Description;
+            $scope.Project = response.Project.Name;
+            $scope.Assigneeid = response.Assignee.Id;
+            $scope.PriorityId = response.Priority.Id;
+            $scope.priorityName = response.Priority.Name;
+            $scope.author = response.Author.Username;
 
+            $scope.status = response.Status.Name;
+            var labels = [];
+            response.Labels.forEach(function (e) {
+                labels.push(e.Name);
+            });
+            $scope.labels = labels.join(', ');
+            var isAdmin = response.Assignee.isAdmin;
+            console.log(response);
+        }, function (error) {
+            console.log(error);
+        })
     }]);
